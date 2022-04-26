b3294d255e6361840a6bceded749385bb3a58d3c
abdulrazzakmohd <84719173+abdulrazzakmohd@users.noreply.github.com>
2022-04-26 18:28:12 +0400

    Initial Commit

---
+++ IOSAssignment/IOSAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAssignmentApp.swift
+//  IOSAssignment
+//
+//  Created by Abdul Razzak on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOSAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
